77a0068262cfeb2f3010fb81cf369e1085d42fc9
Noa Even <69196529+neven-h@users.noreply.github.com>
2023-12-27 13:14:34 +0200

    Add files via upload

---
+++ ps.c
@@ -0,0 +1,15 @@
+// Noa Even 208576256
+// calling the cps156() command defined in the user mode
+
+
+
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
+
+int main(int argc, char *argv[]) {
+	cps156();
+
+	exit();
+}
